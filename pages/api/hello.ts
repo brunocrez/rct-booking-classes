4a147486a492966ed7a71a74361e97e472d1ea99
Bruno Rezende <brunoc.rez@gmail.com>
2021-03-01 17:09:47 -0300

    first commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+interface ResponseType {
+  message: string;
+}
+
+export default (req: NextApiRequest, res: NextApiResponse<ResponseType>): void => {
+  res.status(200).json({ message: 'Everything is alright!' });
+}
