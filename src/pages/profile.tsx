45e3c0596409e33b9fa55d4a0d7b08235fc8584b
Bruno Rezende <brunoc.rez@gmail.com>
2021-03-06 12:22:30 -0300

    finishing registration with profile user

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,18 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import useSWR from 'swr';
 import { signIn, signOut, useSession } from 'next-auth/client';
 
 import { api } from '../../config/api';
 import { Nav } from '../../src/components/Nav';
 
+import styles from '../../styles/pages/Profile.module.css';
+
 export default function Profile() {
   const [ session, loading ] = useSession();
+
+  const [ isTeacher, setIsTeacher ] = useState(false);
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ phone, setPhone ] = useState('');
+  const [ courses, setCourses ] = useState('');
+  const [ availableLocations, setAvailableLocations ] = useState('');
+  const [ monday, setMonday ] = useState(null);
+  const [ tuesday, setTuesday ] = useState(null);
+  const [ wednesday, setWednesday ] = useState(null);
+  const [ thursday, setThursday ] = useState(null);
+  const [ friday, setFriday ] = useState(null);
+
+  const [loggedUserWithoutProfile, setLoggedUserWithoutProfile ] = useState(false);
   
-  const { data, error } = useSWR(`/api/user/${session?.user.email}`, api);
+  const { data, error } = useSWR(
+    !loggedUserWithoutProfile && !loading ? `/api/user/${session?.user.email}` : null, 
+    api);
+
+  useEffect(() => {
+    if (error) {
+      setLoggedUserWithoutProfile(true);
+    }
+  }, [error])
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const available_hours = {
+      monday: monday
+        ?.split(',')
+        .map(item => item.trim())
+        .map(item => parseInt(item)),
+      tuesday: tuesday
+        ?.split(',')
+        .map(item => item.trim())
+        .map(item => parseInt(item)),
+      wednesday: wednesday
+        ?.split(',')
+        .map(item => item.trim())
+        .map(item => parseInt(item)),
+      thursday: thursday
+        ?.split(',')
+        .map(item => item.trim())
+        .map(item => parseInt(item)),
+      friday: friday
+        ?.split(',')
+        .map(item => item.trim())
+        .map(item => parseInt(item)),                                
+    }
+
+    const data = {
+      name,
+      email,
+      phone,
+      courses: courses.split(',').map(item => item.trim()),
+      available_hours,
+      available_locations: availableLocations.split(',').map(item => item.trim())
+    }
+
+    for (const day in available_hours) {
+      if (!available_hours[day]) {
+        delete available_hours[day];
+      }
+    }
+
+    console.log(data)
+    
+    // try {
+    //   await axios.post(`${process.env.NEXT_PUBLIC_URL}/api/user`, data);
+    //   setLoggedUserWithoutProfile(false);
+    // } catch(err) {
+    //   alert(err.response.data.error);
+    // }
+  }
 
   return (
     <div>
       <Nav />
-      <h1>Página Profile</h1>
       {!session && (
         <>
           Favor fazer login para acessar essa página <br/>
@@ -27,7 +103,96 @@ export default function Profile() {
           <p>{data.data.coins}</p>
         </>
       )}
-      {error && <h1>O usuário com e-mail {session.user.email} não existe!</h1>}
+      {loggedUserWithoutProfile && session && (
+        <div className={styles.container}>
+          <h1>Welcome to Booking Classes!</h1>
+          <h2>Please, complete your profile registration</h2>
+
+          <form onSubmit={handleSubmit} className={styles.formContainer}>
+            <input 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              placeholder="Name" />
+            <input 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="E-mail" />
+            <input 
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}            
+              type="cellphone"
+              placeholder="Phone Number" />
+
+            <div>
+              <h4>Are you a Teacher?</h4>
+              <div className={styles.teacherContainer}>
+                <div onClick={() => setIsTeacher(true)}>Yes</div>
+                <div onClick={() => setIsTeacher(false)}>No</div>
+              </div>
+            </div>
+
+            {isTeacher && (
+              <>
+                <h3>Which disciplines will you teach?</h3>
+                <input
+                  value={courses}
+                  onChange={(e) => setCourses(e.target.value)} 
+                  type="text"
+                  placeholder="Funcional, Data" 
+                />
+                <h3>Where can you teach?</h3>
+                <input
+                  value={availableLocations}
+                  onChange={(e) => setAvailableLocations(e.target.value)} 
+                  type="text"
+                  placeholder="Home, Lab Y" 
+                />
+                <h3>When can you teach?</h3>
+                <h4>Monday:</h4>
+                <input
+                  value={monday}
+                  onChange={(e) => setMonday(e.target.value)} 
+                  type="text"
+                  placeholder="7, 20, 21" 
+                />
+                <h4>Tuesday:</h4>
+                <input
+                  value={tuesday}
+                  onChange={(e) => setTuesday(e.target.value)} 
+                  type="text"
+                  placeholder="2PM, 5PM" 
+                />   
+                <h4>Wednesday:</h4>
+                <input
+                  value={wednesday}
+                  onChange={(e) => setWednesday(e.target.value)} 
+                  type="text"
+                  placeholder="2PM, 5PM" 
+                />   
+                <h4>thursday:</h4>
+                <input
+                  value={thursday}
+                  onChange={(e) => setThursday(e.target.value)} 
+                  type="text"
+                  placeholder="2PM, 5PM" 
+                />   
+                <h4>Friday:</h4>
+                <input
+                  value={friday}
+                  onChange={(e) => setFriday(e.target.value)} 
+                  type="text"
+                  placeholder="2PM, 5PM" 
+                />                                                                            
+              </>
+            )}
+
+
+            <button type="submit">Create Profile</button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
